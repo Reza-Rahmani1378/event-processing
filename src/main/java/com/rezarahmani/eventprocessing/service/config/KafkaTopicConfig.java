d08379b0a42988c7c96644fb50fb0c55d4cfd7e2
Reza-Rahmani1378 <reza6273757@gmail.com>
2023-12-12 14:24:48 +0330

    Kafka Topic

---
+++ src/main/java/com/rezarahmani/eventprocessing/service/config/KafkaTopicConfig.java
@@ -0,0 +1,44 @@
+package com.rezarahmani.eventprocessing.service.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+    
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Value(value = "${kafka.topic.impression.name}")
+    private String impressionTopic;
+
+    @Value(value = "${kafka.topic.click.name}")
+    private String clickTopic;
+
+
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+    
+    @Bean
+    public NewTopic impressionEventTopic() {
+         return new NewTopic(impressionTopic, 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic clickEventTopic() {
+         return new NewTopic(clickTopic, 1, (short) 1);
+    }
+
+}
