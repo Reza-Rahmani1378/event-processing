b51202607bec6701d65c32d42ebeee55023c9b02
Reza-Rahmani1378 <reza6273757@gmail.com>
2023-12-12 14:15:21 +0330

    Init Project

---
+++ src/main/java/com/rezarahmani/eventprocessing/EventProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.rezarahmani.eventprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rezarahmani/eventprocessing/EventProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezarahmani.eventprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
