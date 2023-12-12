c9491ac2dafec82edfaa7da71ee99e30996b78c9
Reza-Rahmani1378 <reza6273757@gmail.com>
2023-12-12 13:03:07 +0330

    Init Project

---
+++ todobackend-springdata/src/main/java/com/datasax/workshop/todobackendspringdata/TodobackendSpringdataApplication.java
@@ -0,0 +1,13 @@
+package com.datasax.workshop.todobackendspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodobackendSpringdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodobackendSpringdataApplication.class, args);
+	}
+
+}
---
+++ todobackend-springdata/src/test/java/com/datasax/workshop/todobackendspringdata/TodobackendSpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datasax.workshop.todobackendspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodobackendSpringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
